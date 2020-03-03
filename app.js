bc1779a166e45443a58dd432bd87715f4cb76772
Caleb Gammon <calebwgammon@gmail.com>
2020-03-02 21:23:24 -0500

    success in boredAPI GET req and added SERPAPI key

--- app.js
+++ app.js
@@ -26,36 +26,47 @@ onGetGoogle = function() {
     $('.welcome').hide();
 };
 
-// API Functions here:
-// fetchActivity = function() {}; ... need 2 more for \Free and \accessibility ??
+// API Functions here: Key for SERPAPI: 8408BAE7A9E14665A3F8D5E360C106BB
+fetchActivity = () => {
+    let url = 'http://www.boredapi.com/api/activity';
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {console.log('onFetch error: ', err)});
+}; 
+
 // fetchGoogle = function() {};
 $(document).ready(function() {
     $('.getStarted').on('click', function() {
         console.log('you clicked get started');
         // onGetStarted();
     });
-});
 
-
-$('.getActivity').click(function() {
-    // call Bored App API
-});
-
-$('.findFree').click(function() {
-    // call Bored App API - Free
-});
-
-$('.findEasy').click(function() {
-    // call Bored App API - accessibility
-});
-
-$('.repeat').click(function() {
-    // repeat call Bored App API
-});
-
-$('.googleIt').click(()  => {
-    // call Google API 
+    $('.getActivity').click(function() {
+        // call Bored App API
+    });
+    
+    $('.findFree').click(function() {
+        // call Bored App API - Free
+    });
+    
+    $('.findEasy').click(function() {
+        // call Bored App API - accessibility
+    });
+    
+    $('.repeat').click(function() {
+        // repeat call Bored App API
+    });
+    
+    $('.googleIt').click(()  => {
+        // call Google API 
+    });
 });
 
 
 $(startup);
+$(fetchActivity);
